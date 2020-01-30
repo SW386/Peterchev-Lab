1d35c271fe09e989b6debcc4c16de2314f4500de
Shufan Wen <shufan.wen@duke.edu>
2020-01-30 16:01:59 -0500

    added stream function

---
+++ stream_function.m
@@ -0,0 +1,15 @@
+function k = stream_function(M,L,P,theta,phi)
+    if M>0 
+        const = sqrt((2*L+1)*factorial(L-M)/(2*pi*factorial(L+M)));
+        Yml = const*P(L,M+1)*cos(theta)*cos(M*phi);
+    elseif M==0
+        Yml = sqrt((2*L+1)/(4*pi))*P(1,1)*cos(theta);
+    else 
+        const = -sqrt((2*L+1)*factorial(L+M)/(2*pi*factorial(L-M)));
+        Yml = const*P(L,-M+1)*cos(theta)*sin(M*phi);
+    end
+    k = Yml/(sqrt(L*(L+1)));
+end
+
+
+
