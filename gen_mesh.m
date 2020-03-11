0b86a2695a2fa223db0d5b7a647fbdd2c9ec60f7
Shufan Wen <shufan.wen@duke.edu>
2020-03-11 15:46:18 -0400

    mesh

--- gen_mesh.m
+++ gen_mesh.m
@@ -1,8 +1,39 @@
+clear all
 [X,Y,Z] = ndgrid(0:0.01:0.32 , 0:0.01:0.32,0);
 tri = delaunay(X,Y);
 P(:,1) = X(:);
 P(:,2) = Y(:);
 P(:,3) = Z(:);
+hold on
 
 [RGW,centroid] = gen_RWG(P,tri);
-E = gen_mesh_field(RGW,centroid,[1,1,0]);
+RGW=reshape(permute(RGW,[2 1 3]),[numel(tri) 3]);
+%E = gen_mesh_field(RGW,centroid,[1,1,0]);
+[~,~,~,~,inodes,Aloop,Arwg,~,~,~]=extractmesharrays(tri,P);
+Aloop=Arwg*Aloop(:,inodes);
+[~,cc]=ndgrid(1:3,1:numel(tri)/3);
+
+FEMord=1;
+reflev=0;
+load(strcat('C:\Users\Shufan Wen\Desktop\Lab\TMS_Efield_Solvers\samplescenario',num2str(reflev),'.mat'));
+E_field = zeros(3,2,size(Aloop,2));
+r = [0 0;
+    0 0;
+    0.0849 0.07];
+for j = 1:size(Aloop,2)
+    ns=nnz(Aloop(:,j));
+    rs=zeros([3 ns]);
+    js=zeros([3 ns]);
+    ct=1;
+    for i=1:numel(Aloop(:,1))
+        if Aloop(i,j)~=0
+            rs(1:3,ct)=transpose(centroid(cc(i),1:3));
+            js(1:3,ct)=Aloop(i,j)*RGW(i,1:3);
+            ct=ct+1;
+        end
+    end
+    [E1,~,~,~,~]=runcode(te2p',p',conductivity,rs,js,r,FEMord);
+    E_field(:,:,j) = E1;
+end
+
+
